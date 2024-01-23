7a7a9ab11d5f282f08b713dc825abbc3b6583dd5
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-01-23 16:01:31 +0100

    Création d'un React Component de menu de navigation

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h3>App Ideas</h3>
+      <p>Find your next App Idea right here</p>
+    </main>
+  );
+}
