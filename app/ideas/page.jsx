d69d8c55492350f5a83d1448afaf330847bbba06
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-01-23 18:07:01 +0100

    CSS module de la page 'Ideas'

--- app/ideas/page.jsx
+++ app/ideas/page.jsx
@@ -1,8 +1,35 @@
+import styles from './ideas.module.css';
+
 export default function Ideas() {
     return (
         <main className="main">
             <h3>Ideas</h3>
-            <p>All Ideas</p>
+            <div className={styles.container}>
+                <div className={styles.idea}>
+                    <div className={styles['idea-title']}>Create the new Twitter</div>
+                    <div className={styles['idea-description']}>
+                        As Twitter is dying, it's the perfect moment to create a new one
+                    </div>
+                </div>
+                <div className={styles.idea}>
+                    <div className={styles['idea-title']}>Map Browser Extension</div>
+                    <div className={styles['idea-description']}>
+                        Create a Browser extension that opens a popup displaying a map of the first address appearing in a web page
+                    </div>
+                </div>
+                <div className={styles.idea}>
+                    <div className={styles['idea-title']}>Motorbike Site</div>
+                    <div className={styles['idea-description']}>
+                        Create a website that helps riders to find their new motorbike
+                    </div>
+                </div>
+                <div className={styles.idea}>
+                    <div className={styles['idea-title']}>React and Next.js Blog</div>
+                    <div className={styles['idea-description']}>
+                        Create a Blog devoted to React and Next
+                    </div>
+                </div>
+            </div>
         </main>
     )
 }
