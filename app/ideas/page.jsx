6a7addc72eef3ac91e3c6f1aef2940c1eafe190c
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-01-24 09:27:57 +0100

    Rendu du tableau d'Ideas

--- app/ideas/page.jsx
+++ app/ideas/page.jsx
@@ -11,35 +11,14 @@ export default async function Ideas() {
     // console.log( ideas );
     return (
         <main className="main">
-            <h3>Ideas</h3>
             <div className={styles.container}>
-                <div className={styles.idea}>
-                    <div className={styles['idea-title']}>Create the new Twitter</div>
-                    <div className={styles['idea-description']}>
-                        As Twitter is dying, it's the perfect moment to create a new one
-                    </div>
+            {ideas.length && ideas.map((idea) => (
+                <div className={styles.idea} key={idea.id}>
+                    <div className={styles['idea-title']}>{idea.title}</div>
+                    <div className={styles['idea-description']}>{idea.description}</div>
                 </div>
-                <div className={styles.idea}>
-                    <div className={styles['idea-title']}>Map Browser Extension</div>
-                    <div className={styles['idea-description']}>
-                        Create a Browser extension that opens a popup displaying a map of the first address appearing in a web page
-                    </div>
-                </div>
-                <div className={styles.idea}>
-                    <div className={styles['idea-title']}>Motorbike Site</div>
-                    <div className={styles['idea-description']}>
-                        Create a website that helps riders to find their new motorbike
-                    </div>
-                </div>
-                <div className={styles.idea}>
-                    <div className={styles['idea-title']}>React and Next.js Blog</div>
-                    <div className={styles['idea-description']}>
-                        Create a Blog devoted to React and Next
-                    </div>
-                </div>
+            ))}
             </div>
-            {/* Va chercher le json à partir de l'api ideas pour l'afficher sur la page */}
-            {JSON.stringify(ideas, null, 2)}
         </main>
     )
 }
