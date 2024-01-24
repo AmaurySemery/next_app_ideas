1ac49bc777aa04d61f86d2d91a35b546f8628f88
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-01-24 09:10:28 +0100

    Créer une API REST

---
+++ app/api/ideas/route.js
@@ -0,0 +1,7 @@
+import { NextResponse } from "next/server";
+import ideas from './ideas.json'
+
+export async function GET(request) {
+    console.log({url: request.url, request});
+    return NextResponse.json(ideas);
+}
