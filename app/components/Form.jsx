b0342e1e595f725e5793ea2591223ba8f4f6b3a6
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-01-25 15:34:03 +0100

    Gérer la soumission du formulaire depuis un 'Client Component' vers un 'Server Component' React

--- app/components/Form.jsx
+++ app/components/Form.jsx
@@ -1,16 +1,39 @@
-export default function Form({ formTitle }) {
-    return (
-        <div className="form-container">
-            <h3 className='form-header'>{formTitle}</h3>
-            <form className="form">
-                <label htmlFor="title">Idea Title</label>
-                <input type="text" id="title" />
+'use client';
 
-                <label htmlFor="description">Idea Description</label>
-                <textarea name="description" id="description" cols="30" rows="5">
-                </textarea>
-                <input type="submit" value="Create" className="btn" />
-            </form>
-        </div>
-    )
+import { useState } from "react"
+
+export default function Form({ formTitle, addIdea }) {
+    const [ideaTitle, setIdeaTitle] = useState('');
+    const [ideaDescription, setIdeaDescription] = useState('');
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        addIdea(ideaTitle, ideaDescription);
+    }
+
+    return (
+      <div className="form-container">
+        <h3 className="form-header">{formTitle}</h3>
+        <form className="form" onSubmit={handleSubmit}>
+          <label htmlFor="title">Idea Title</label>
+          <input
+            type="text"
+            id="title"
+            value={ideaTitle}
+            onInput={(e) => setIdeaTitle(e.target.value)}
+          />
+
+          <label htmlFor="description">Idea Description</label>
+          <textarea
+            name="description"
+            id="description"
+            cols="30"
+            rows="5"
+            value={ideaDescription}
+            onInput={e => setIdeaDescription(e.target.value)}
+          ></textarea>
+          <input type="submit" value="Create" className="btn" />
+        </form>
+      </div>
+    );
 }
